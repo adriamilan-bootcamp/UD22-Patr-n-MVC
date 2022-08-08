1b87427ca830a1ad85d34c50411fa8c24eb7f7e8
ionela vladasel <ionelavladasel2710@gmail.com>
2022-08-08 11:35:46 +0200

    just testing

--- tarea1/src/main/java/AppMain/App.java
+++ tarea1/src/main/java/AppMain/App.java
@@ -1,13 +1,17 @@
 package AppMain;
 
+import java.sql.Date;
+
 import models.DBConnection;
+import models.ModelClients;
 
 public class App {
 
 	public static void main(String[] args) {
-
+		ModelClients model = new ModelClients();
 		DBConnection conexion = new DBConnection();
 		conexion.useDataBase("ud22_01");
+		model.insertClient("maria", "antonieta", "calle pepe,nr3", 12345678, Date.valueOf("2022-05-12"));
 		// modelos,vistas y el controlador ->llamar
 		conexion.closeConnection();
 	}
--- tarea1/src/main/java/models/ModelClients.java
+++ tarea1/src/main/java/models/ModelClients.java
@@ -3,6 +3,7 @@ package models;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 import javax.swing.JOptionPane;
 
@@ -15,13 +16,15 @@ public class ModelClients {
 	 * @param db       nombre de la base datos donde se insertaran los datos
 	 * @param SQLquery sentencia sql de la insercion de los datos en la tabla
 	 */
-	public void insertClient(String SQLquery) {
+
+	public void insertClient(String nombre, String apellido, String direccion, int dni, Date fecha) {
 
 		try {
-
+			String SQLquery = "insert into clients(nombre,apellido,direccion,dni,fecha) values (" + nombre + ","
+					+ apellido + "," + direccion + "," + dni + "," + fecha + ");";
 			Statement st = conexion.createStatement();
 			st.executeUpdate(SQLquery);
-			System.out.println("Insert sobre la tabla realizado correctamente");
+			System.out.println("El cliente a sido guardado correctamente");
 		} catch (SQLException ex) {
 			JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
 		}
