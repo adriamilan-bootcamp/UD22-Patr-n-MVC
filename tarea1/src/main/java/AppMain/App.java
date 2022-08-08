e81e9cef3b652eb38f6733a7a2be12560b0e771d
ionela vladasel <ionelavladasel2710@gmail.com>
2022-08-08 10:51:01 +0200

    created the structure initial

--- tarea1/src/main/java/AppMain/App.java
+++ tarea1/src/main/java/AppMain/App.java
@@ -3,27 +3,12 @@ package AppMain;
 import models.DBConnection;
 
 public class App {
-		
-	public static void main(String[] args) {
-		
-		DBConnection conexion = new DBConnection();			
-		
-		structureCreation(conexion);
-		
-		conexion.closeConnection();		
-	}
-	
-	/**
-	* Metodo donde se realiza la creacion de la estructura mysql con datos estaticos
-	*/
-	public static void structureCreation(DBConnection conexion) {
-		
-		String dbName = "UD22_Tarea01";
-		
-		conexion.createDB(dbName); 
-		
-		conexion.createTable(dbName, "CREATE TABLE Cliente (id int(11) NOT NULL AUTO_INCREMENT, nombre varchar(250) DEFAULT NULL, apellido varchar(250) DEFAULT NULL, direccion varchar(250) DEFAULT NULL, dni int(11) DEFAULT NULL, fecha date DEFAULT NULL, PRIMARY KEY (id));");
 
+	public static void main(String[] args) {
+
+		DBConnection conexion = new DBConnection();
+		// modelos,vistas y el controlador ->llamar
+		conexion.closeConnection();
 	}
-	
+
 }
--- tarea1/src/main/java/models/ModelClients.java
+++ tarea1/src/main/java/models/ModelClients.java
@@ -1,5 +1,33 @@
 package models;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
 public class ModelClients {
+	Connection conexion;
+
+	/**
+	 * Metodo que inserta datos en una tabla
+	 * 
+	 * @param db       nombre de la base datos donde se insertaran los datos
+	 * @param SQLquery sentencia sql de la insercion de los datos en la tabla
+	 */
+	public void insertData(String db, String SQLquery) {
+
+		try {
+			String Querydb = "USE " + db + ";";
+			Statement stdb = conexion.createStatement();
+			stdb.executeUpdate(Querydb);
+
+			Statement st = conexion.createStatement();
+			st.executeUpdate(SQLquery);
+			System.out.println("Insert sobre la tabla realizado correctamente");
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
+		}
+	}
 
 }
